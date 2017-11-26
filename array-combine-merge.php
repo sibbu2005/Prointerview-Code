c4508fc6d932236157a6f0769412f408c4a843f5
sibbu2005 <sibbu2005@gmail.com>
2017-11-27 00:27:07 +0530

    Update array-combine-merge.php

--- array-combine-merge.php
+++ array-combine-merge.php
@@ -1,4 +1,11 @@
 <?php
-$employee=array('Dhananjay','Peter','Himanshu',Ashish);
+$employee=array('Dhananjay','Peter','Himanshu','Ashish');
 $emp_code=array('001','002','003','004');
+
+$array_merge=array_merge($employee,$emp_code);
+//RESULTING ARRAY
+/*
+ Array('Dhananjay','Peter','Himanshu','Ashish','001','002','003','004')
+*/
+
 ?>
